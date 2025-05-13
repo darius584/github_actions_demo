1e30f2a56a7c5db940b8f2da383958a504232a84
darius584 <darius.spruogis@cognizant.com>
2025-05-13 11:49:35 +0300

    update workflow

--- .github/workflows/actions/node-action/main.js
+++ .github/workflows/actions/node-action/main.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core')
+const exec = require('@actions/exec')
 function fun() {
     const msg = core.getInput('msg')
+    exec.exec('echo echoing')
     core.notice(msg)    
 }
 fun();
